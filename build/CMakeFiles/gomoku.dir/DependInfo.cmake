f958638db640118db130090adc774eaebdfebea1
Yihe Huang <yih2943997328@outlook.com>
2026-05-10 20:28:44 +0800

    v1:基本功能实现，存在假胜策略需要修复

---
+++ build/CMakeFiles/gomoku.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Yihe Huang/TJU/traditional_gomoku/src/board.cpp" "CMakeFiles/gomoku.dir/src/board.cpp.obj" "gcc" "CMakeFiles/gomoku.dir/src/board.cpp.obj.d"
+  "C:/Users/Yihe Huang/TJU/traditional_gomoku/src/evaluator.cpp" "CMakeFiles/gomoku.dir/src/evaluator.cpp.obj" "gcc" "CMakeFiles/gomoku.dir/src/evaluator.cpp.obj.d"
+  "C:/Users/Yihe Huang/TJU/traditional_gomoku/src/game.cpp" "CMakeFiles/gomoku.dir/src/game.cpp.obj" "gcc" "CMakeFiles/gomoku.dir/src/game.cpp.obj.d"
+  "C:/Users/Yihe Huang/TJU/traditional_gomoku/src/main.cpp" "CMakeFiles/gomoku.dir/src/main.cpp.obj" "gcc" "CMakeFiles/gomoku.dir/src/main.cpp.obj.d"
+  "C:/Users/Yihe Huang/TJU/traditional_gomoku/src/movegen.cpp" "CMakeFiles/gomoku.dir/src/movegen.cpp.obj" "gcc" "CMakeFiles/gomoku.dir/src/movegen.cpp.obj.d"
+  "C:/Users/Yihe Huang/TJU/traditional_gomoku/src/pattern.cpp" "CMakeFiles/gomoku.dir/src/pattern.cpp.obj" "gcc" "CMakeFiles/gomoku.dir/src/pattern.cpp.obj.d"
+  "C:/Users/Yihe Huang/TJU/traditional_gomoku/src/search.cpp" "CMakeFiles/gomoku.dir/src/search.cpp.obj" "gcc" "CMakeFiles/gomoku.dir/src/search.cpp.obj.d"
+  "C:/Users/Yihe Huang/TJU/traditional_gomoku/src/threat.cpp" "CMakeFiles/gomoku.dir/src/threat.cpp.obj" "gcc" "CMakeFiles/gomoku.dir/src/threat.cpp.obj.d"
+  "C:/Users/Yihe Huang/TJU/traditional_gomoku/src/transposition.cpp" "CMakeFiles/gomoku.dir/src/transposition.cpp.obj" "gcc" "CMakeFiles/gomoku.dir/src/transposition.cpp.obj.d"
+  "C:/Users/Yihe Huang/TJU/traditional_gomoku/src/ui_win.cpp" "CMakeFiles/gomoku.dir/src/ui_win.cpp.obj" "gcc" "CMakeFiles/gomoku.dir/src/ui_win.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/gomoku.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gomoku.dir/src/board.cpp.obj"
+  "CMakeFiles/gomoku.dir/src/board.cpp.obj.d"
+  "CMakeFiles/gomoku.dir/src/evaluator.cpp.obj"
+  "CMakeFiles/gomoku.dir/src/evaluator.cpp.obj.d"
+  "CMakeFiles/gomoku.dir/src/game.cpp.obj"
+  "CMakeFiles/gomoku.dir/src/game.cpp.obj.d"
+  "CMakeFiles/gomoku.dir/src/main.cpp.obj"
+  "CMakeFiles/gomoku.dir/src/main.cpp.obj.d"
+  "CMakeFiles/gomoku.dir/src/movegen.cpp.obj"
+  "CMakeFiles/gomoku.dir/src/movegen.cpp.obj.d"
+  "CMakeFiles/gomoku.dir/src/pattern.cpp.obj"
+  "CMakeFiles/gomoku.dir/src/pattern.cpp.obj.d"
+  "CMakeFiles/gomoku.dir/src/search.cpp.obj"
+  "CMakeFiles/gomoku.dir/src/search.cpp.obj.d"
+  "CMakeFiles/gomoku.dir/src/threat.cpp.obj"
+  "CMakeFiles/gomoku.dir/src/threat.cpp.obj.d"
+  "CMakeFiles/gomoku.dir/src/transposition.cpp.obj"
+  "CMakeFiles/gomoku.dir/src/transposition.cpp.obj.d"
+  "CMakeFiles/gomoku.dir/src/ui_win.cpp.obj"
+  "CMakeFiles/gomoku.dir/src/ui_win.cpp.obj.d"
+  "gomoku.exe"
+  "gomoku.exe.manifest"
+  "gomoku.pdb"
+  "libgomoku.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gomoku.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
